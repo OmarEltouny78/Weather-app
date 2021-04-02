7e389a7459949051bf632427ab7fa4b8a274799a
OmarEltouny78 <omar.eltouny@uofcanada.edu.eg>
2021-04-02 15:18:50 +0200

    Update app.js

--- weather-journal-app/website/app.js
+++ weather-journal-app/website/app.js
@@ -1,68 +1,64 @@
-/* Global Variables */
-
-// Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 
-let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
-let key = '661daa7377189bfe425b6af1f07ac279';
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '&appid=5beb43e13dc3fa8be428191e6e50a00c';
 
-document.getElementById('generate').addEventListener('click',performAction);
 
-function performAction(e){
-    const postCode=document.getElementById('zip').value;
-    const feelings=document.getElementById('feelings').value;
-    console.log(newDate);
-    getTemperature(baseURL,postCode,key)
-    .then(function (data){
-        // Add data to POST request
-        postData('http://localhost:8080/addWeatherData', {temperature: data.main.temp, date: newDate, user_response: feelings } )
-        // Function which updates UI
-        .then(function() {
-            updateUI()
-        })
+document.getElementById('generate').addEventListener('click',perform);
+
+function perform(){
+    const newZip = document.getElementById('zip').value;
+    const content = document.getElementById('feelings').value;
+    getWthr(baseURL,newZip,apiKey)
+    .then(function (userData) {
+      // add data to POST request
+      postData('http://localhost:3000/all', { date: newDate, temp: userData.main.temp, content })
+    }).then(function () {
+        UI_Update()
     })
+
 }
 
-const getTemperature=async (baseURL,code,key)=>{
-    const response = await fetch(baseURL + code + ',us' + '&APPID=' + key)
-    console.log(response);
-    try{
-        const data=await response.json();
-        console.log(data);
-        return data;
-    }catch(error){
-        console.log(error);
+const getWthr = async(baseURL,newZip,apiKey)=>{
+    const res=await(baseURL+newZip+apiKey);
+    try {
+        const userData=await res.join();
+        return userData;
+    }catch(e){
+        console.log(e);
     }
 }
 
-
-const postData = async (url = '', data = {}) =>{
-    const postRequest = await fetch(url, {
-        method: 'POST',
-        credentials: 'same-origin',
+const postData= async(url='',data={})=>{
+    const req = await fetch(url, {
+        method: "POST",
+        credentials: "same-origin",
         headers: {
-            'Content-Type': 'application/json',
+          "Content-Type": "application/json;charset=UTF-8"
         },
-        body: JSON.stringify(data),
-    });
+        body: JSON.stringify({
+          date: data.date,
+          temp: data.temp,
+          content: data.content
+        })
+      })
+      try{
+          const data=await req.join();
+          return data;
+      }catch(e){
+          console.log(e);
+      }
+};
+const UI_Update=async()=>{
+    const request=await fetch('http://localhost:3000/all');
     try{
-        const newData=await postRequest.json();
-        return newData;
-    }catch(error){
-        console.log(error);
-    }
-}
+        const data= await request.join()
 
-const updateUI = async() =>{
-    const request = await fetch('http://localhost:3000/all');
-    try{
-        const allData= await request.json();
         document.getElementById('date').innerHTML = allData.date;
         document.getElementById('temp').innerHTML = allData.temperature;
-        document.getElementById('content').innerHTML = allData.user_response;
-    }catch(error){
-        console.log(error);
+        document.getElementById('content').innerHTML = allData.content;
+    }catch(e){
+        console.log(e);
     }
-
 }
